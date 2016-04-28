cd1aad7737e3100bbf498a6bde45c8027968345a
hieudt7 <hieudtx9@gmail.com>
2016-04-29 00:12:14 +0700

    test url

---
+++ scripts/common.js
@@ -0,0 +1,10 @@
+﻿$(function () {
+    $('.Calltooltip').tooltip();
+})
+$(window).bind("load", function () {
+    var left = $('.menu-left').height();
+    var right = $('.content-right').height();
+    if (left < right) {
+        $('.menu-left').css('height', right);
+    }
+})
